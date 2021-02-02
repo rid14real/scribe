f3c0a7a40bdeb2ed39bd6d44e73c092f147b4195
rid14real <62150969+rid14real@users.noreply.github.com>
2021-02-02 03:12:31 +0100

    Using if to print a specific plant

---
+++ basic if-else.py
@@ -0,0 +1,7 @@
+right_plant = input("Insert a plant:")
+if right_plant == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+if right_plant == "pelargonium":
+    print("Spathyphyllum! Not pelargonium!")
+if right_plant == "Spathiphyllum":
+    print("Yes = Spathiphyllum is the best plant ever!")
